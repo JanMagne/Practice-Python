5447a496ee77768259bc3fcf9985d94c4d9ce1f2
JanMagne <49604808+JanMagne@users.noreply.github.com>
2019-05-04 21:34:34 +0200

    Add files via upload
    Updated game.py to actually run the game

--- 29/game.py
+++ 29/game.py
@@ -7,15 +7,38 @@ from UserInput import *
 
 
 
+def getNewGame():
+	return [[0,0,0],
+		    [0,0,0],
+		    [0,0,0]]
 
+def welcome():
+	print("Welcome to Tic-Tac-Toe")
+	print("You are gonna be asked to input the coordinates for your square")
+	print("Please enter them as coordinates like (x,y) from 1 to 3")
+	print("Player 1 plays as 'x', player to plays as 'o'")
+	print("Here we go:\n")
+	printGameBoard(getNewGame())
 
 def main():
-	playing = True
-	printGameBoard(3)
+	welcome()
+	game = getNewGame()
+	whosTurn = 1
+	
+	while 1:
+		game = usrInp(game, whosTurn)
+		printGameBoard(game)
 
-	while playing:
-		
+		if checkForWinner(game):
+			print("The winner is " + checkForWinner(game))
+			break
+
+		if whosTurn == 1:
+			whosTurn = 2
+		elif whosTurn == 2:
+			whosTurn = 1
 
 
+	print("Thanks for playing. Please come again! \n- Jannis, 2019")
 if __name__ == '__main__':
 	main()
